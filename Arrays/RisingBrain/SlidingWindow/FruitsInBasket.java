ded6273dab3117d5906ef1f53aa8c9ca927d2fde
Hemanth-1524 <chintu19111973@gmail.com>
2026-07-13 11:06:16 +0530

    Few more problems added

---
+++ Arrays/RisingBrain/SlidingWindow/FruitsInBasket.java
@@ -0,0 +1,30 @@
+
+import java.util.HashMap;
+
+public class FruitsInBasket {
+
+    public int totalFruit(int[] fruits) {
+
+        int n = fruits.length;
+        int ans = 0;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+            map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
+
+            while (map.size() > 2) {
+                map.put(fruits[left], map.get(fruits[left]) - 1);
+                if (map.get(fruits[left]) == 0) {
+                    map.remove(fruits[left]);
+                }
+                left++;
+            }
+
+            ans = Math.max(ans, right - left + 1);
+        }
+        return ans;
+
+    }
+}
